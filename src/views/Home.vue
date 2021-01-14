17191ee3fc65d634628add2aef4e69646d7f2567
Netto Ramalho <netto.ramalho@gmail.com>
2021-01-14 14:09:00 -0300

    Preparing pages and component structures

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="home">
     <h1 class="h1">SW Universe</h1>
+    <SearchField />
+    <PeopleTable />
   </div>
 </template>
 
 <script>
+import SearchField from "@/components/SearchField";
+import PeopleTable from "@/components/PeopleTable";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    SearchField,
+    PeopleTable
+  }
 };
 </script>
